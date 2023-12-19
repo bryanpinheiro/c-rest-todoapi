033e0468b88ca8ff6629f3780e4e112c6005e382
Bryan Pinheiro <bryansouza_01@outlook.com>
2023-12-19 20:52:58 -0300

    Returning JSON example

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 #include <string.h>
 #include <stdio.h>
 
-#define PAGE "<html><head><title>Hello C api</title>" \
-             "</head><body>C api made by Bryan Pinheiro</body></html>"
+#define HTML_RESPONSE "<html><head><title>Hello C api</title></head><body>C api made by Bryan Pinheiro</body></html>"
+#define JSON_RESPONSE "{\"message\": \"This is a JSON response.\"}"
 
 static enum MHD_Result
 ahc_echo(void *cls,
@@ -17,28 +17,40 @@ ahc_echo(void *cls,
          void **ptr)
 {
   static int dummy;
-  const char *page = cls;
   struct MHD_Response *response;
   int ret;
 
   if (0 != strcmp(method, "GET"))
     return MHD_NO; /* unexpected method */
-  if (&dummy != *ptr)
+
+  if (strcmp(url, "/api/") == 0)
   {
-    /* The first time only the headers are valid,
-       do not respond in the first round... */
-    *ptr = &dummy;
-    return MHD_YES;
+    response = MHD_create_response_from_buffer(strlen(JSON_RESPONSE),
+                                               (void *)JSON_RESPONSE,
+                                               MHD_RESPMEM_PERSISTENT);
+    MHD_add_response_header(response, "Content-Type", "application/json");
+    ret = MHD_queue_response(connection,
+                             MHD_HTTP_OK,
+                             response);
+    MHD_destroy_response(response);
+    return ret;
   }
-  if (0 != *upload_data_size)
-    return MHD_NO; /* upload data in a GET!? */
-  *ptr = NULL;     /* clear context pointer */
-  response = MHD_create_response_from_buffer(strlen(page),
-                                             (void *)page,
-                                             MHD_RESPMEM_PERSISTENT);
-  ret = MHD_queue_response(connection,
-                           MHD_HTTP_OK,
-                           response);
+
+  if (strcmp(url, "/") == 0)
+  {
+    response = MHD_create_response_from_buffer(strlen(HTML_RESPONSE),
+                                               (void *)HTML_RESPONSE,
+                                               MHD_RESPMEM_PERSISTENT);
+    ret = MHD_queue_response(connection,
+                             MHD_HTTP_OK,
+                             response);
+    MHD_destroy_response(response);
+    return ret;
+  }
+
+  /* If URL not found, return a 404 */
+  response = MHD_create_response_from_buffer(0, NULL, MHD_RESPMEM_PERSISTENT);
+  ret = MHD_queue_response(connection, MHD_HTTP_NOT_FOUND, response);
   MHD_destroy_response(response);
   return ret;
 }
@@ -47,21 +59,24 @@ int main(int argc,
          char **argv)
 {
   struct MHD_Daemon *d;
+
   if (argc != 2)
   {
-    printf("%s PORT\n",
-           argv[0]);
+    printf("%s PORT\n", argv[0]);
     return 1;
   }
+
   d = MHD_start_daemon(MHD_USE_THREAD_PER_CONNECTION,
                        atoi(argv[1]),
                        NULL,
                        NULL,
                        &ahc_echo,
-                       PAGE,
+                       HTML_RESPONSE,
                        MHD_OPTION_END);
+
   if (d == NULL)
     return 1;
+
   (void)getc(stdin);
   MHD_stop_daemon(d);
   return 0;
